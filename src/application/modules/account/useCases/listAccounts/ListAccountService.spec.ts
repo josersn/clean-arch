0e711a49135ebfdca0ad4e5a1ea463b05f3b668c
josersn <nettorammos@hotmail.com>
2022-03-17 12:15:05 -0300

    test: Should be able to list all company account

---
+++ src/application/modules/account/useCases/listAccounts/ListAccountService.spec.ts
@@ -0,0 +1,44 @@
+import { AccountRepository } from "../../repositories/AccountRepository";
+import { CreateAccountService } from "../createAccount/CreateAccountService";
+import { ListAccountService } from "./ListAccountService";
+
+let sut: ListAccountService;
+let rut: AccountRepository;
+let createAccountService: CreateAccountService;
+beforeAll(() => {
+    rut = new AccountRepository();
+    sut = new ListAccountService(rut);
+    createAccountService = new CreateAccountService(rut);
+
+})
+
+describe('List all company accounts service', () => {
+    it('Should be able to list all company account', async () => {
+
+        const firstCompanyData = {
+            name: "vvlog",
+            cnpj: "04221023000500",
+            description: "A transport company in Via's group",
+            logo: "http://www.setcesp.org.br/upload/ckeditor/files/SetcespOnline/Associados/VVLog_-_viavarejo.com.brvvlog.png",
+            address: "Av. Conde Francisco Matarazzo",
+            revenue: 100000
+        }
+
+        const secondCompanyData = {
+            name: "via",
+            cnpj: "33041260065290",
+            description: "A Company group",
+            logo: "https://www.via.com.br/",
+            address: "Av. Conde Francisco Matarazzo",
+            revenue: 100000
+        }
+
+        const firstAccount = await createAccountService.execute(firstCompanyData);
+        const secondAccount = await createAccountService.execute(secondCompanyData);
+
+        const accountList = await sut.execute();
+
+        expect(accountList).toEqual([firstAccount, secondAccount]);
+
+    })
+})
