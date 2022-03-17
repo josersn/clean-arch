eade286e8576074fe460a67349f363b400446c80
josersn <nettorammos@hotmail.com>
2022-03-17 11:06:07 -0300

    test: Should not be able to create a company with invalid CPNJ

--- src/application/modules/account/useCases/createAccount/CreateAccountService.spec.ts
+++ src/application/modules/account/useCases/createAccount/CreateAccountService.spec.ts
@@ -1,14 +1,17 @@
+import { AccountValidator } from "../../helpers/AccountValidators";
 import { AccountRepository } from "../../repositories/AccountRepository";
 import { CreateAccountService } from "./CreateAccountService";
 
 let rut: AccountRepository;
 let sut: CreateAccountService;
+let accountValidator: AccountValidator;
 
 describe('Create a company account use case.', () => {
 
   beforeEach(() => {
-    rut = new AccountRepository();
-    sut = new CreateAccountService(rut);
+    accountValidator = new AccountValidator();
+    rut = new AccountRepository();       // repository under test
+    sut = new CreateAccountService(rut, accountValidator); // service under test
   })
 
   it('Should be able to a new create account.', async () => {
@@ -30,7 +33,6 @@ describe('Create a company account use case.', () => {
 
   it("Should not be able to create a company with exists cnpj", async () => {
 
-
     const companyData = {
       name: "vvlog",
       cnpj: "04221023000500",
@@ -51,5 +53,20 @@ describe('Create a company account use case.', () => {
     })).rejects.toThrowError(Error("User Already Exists"));
 
   });
+
+  it("Should not be able to create a company with invalid CPNJ", async () => {
+
+    const companyData = {
+      name: "vvlog",
+      cnpj: "01011010101510",
+      description: "A transport company in Via's group",
+      logo: "http://www.setcesp.org.br/upload/ckeditor/files/SetcespOnline/Associados/VVLog_-_viavarejo.com.brvvlog.png",
+      address: "Av. Conde Francisco Matarazzo",
+      revenue: 100000
+    }
+
+    await expect(sut.execute(companyData)).rejects.toThrowError("Company CPNJ is invalid")
+
+  })
 
 })  
