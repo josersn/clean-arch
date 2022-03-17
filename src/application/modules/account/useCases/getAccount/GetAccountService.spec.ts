2d0e6c6300ea13f07fc7aaa3b0375271a64a531d
josersn <nettorammos@hotmail.com>
2022-03-17 11:42:00 -0300

    test: Should able to show a company account

---
+++ src/application/modules/account/useCases/getAccount/GetAccountService.spec.ts
@@ -0,0 +1,40 @@
+import { AccountRepository } from "../../repositories/AccountRepository";
+import { CreateAccountService } from "../createAccount/CreateAccountService";
+import { GetAccountService } from "./GetAccountService";
+
+
+let sut: GetAccountService;
+let rut: AccountRepository;
+let createAccountService: CreateAccountService;
+
+beforeEach(() => {
+
+    rut = new AccountRepository();
+    sut = new GetAccountService(rut);
+    createAccountService = new CreateAccountService(rut);
+
+})
+
+
+describe('List a Company Account Service', () => {
+    it('Should able to show a company account', async () => {
+
+        const companyData = {
+            name: "vvlog",
+            cnpj: "04221023000500",
+            description: "A transport company in Via's group",
+            logo: "http://www.setcesp.org.br/upload/ckeditor/files/SetcespOnline/Associados/VVLog_-_viavarejo.com.brvvlog.png",
+            address: "Av. Conde Francisco Matarazzo",
+            revenue: 100000
+        }
+
+        const account = await createAccountService.execute(companyData);
+
+        const accountFound = await sut.execute(account.id);
+
+
+        expect(accountFound).toBeTruthy();
+        expect(accountFound?.cnpj).toBe(companyData.cnpj);
+
+    })
+})
