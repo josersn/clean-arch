ec7ac542a63d4fbb9122e82175970a3f9a2488ec
josersn <nettorammos@hotmail.com>
2022-03-17 14:44:56 -0300

    feat: Should not be able to edit a non exist account

--- src/application/modules/account/useCases/editAccount/EditAccountService.spec.ts
+++ src/application/modules/account/useCases/editAccount/EditAccountService.spec.ts
@@ -44,4 +44,18 @@ describe('Edit Account company service ', () => {
         expect(accountFound).toEqual(newCompanyData);
 
     })
+
+    it('Should not be able to edit a non exist account', async () => {
+
+        const newCompanyData = {
+            name: "BanQi",
+            cnpj: "04221023000500",
+            description: "A digital bank in Via's group",
+            logo: "https://banqi.com.br/assets/img/ui/logo-internas.svg",
+            address: "Av. Conde Francisco Matarazzo",
+            revenue: 100000
+        }
+
+        await expect(sut.execute('MOCK_ID', newCompanyData)).rejects.toThrowError("Account not exists");
+    })
 })
