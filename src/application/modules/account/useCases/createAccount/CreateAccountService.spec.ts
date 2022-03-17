6aa64e820aaecad86d7afa61c6d9a793a0710514
josersn <nettorammos@hotmail.com>
2022-03-17 09:58:13 -0300

    test: Should not be able to create a company with exists cnpj

---
+++ src/application/modules/account/useCases/createAccount/CreateAccountService.spec.ts
@@ -0,0 +1,55 @@
+import { AccountRepository } from "../../repositories/AccountRepository";
+import { CreateAccountService } from "./CreateAccountService";
+
+let rut: AccountRepository;
+let sut: CreateAccountService;
+
+describe('Create a company account use case.', () => {
+
+  beforeEach(() => {
+    rut = new AccountRepository();
+    sut = new CreateAccountService(rut);
+  })
+
+  it('Should be able to a new create account.', async () => {
+
+    const companyData = {
+      name: "vvlog",
+      cnpj: "04221023000500",
+      description: "A transport company in Via's group",
+      logo: "http://www.setcesp.org.br/upload/ckeditor/files/SetcespOnline/Associados/VVLog_-_viavarejo.com.brvvlog.png",
+      address: "Av. Conde Francisco Matarazzo",
+      revenue: 100000
+    }
+    const response = await sut.execute(companyData);
+
+    expect(response).toBeTruthy();
+  })
+
+
+
+  it("Should not be able to create a company with exists cnpj", async () => {
+
+
+    const companyData = {
+      name: "vvlog",
+      cnpj: "04221023000500",
+      description: "A transport company in Via's group",
+      logo: "http://www.setcesp.org.br/upload/ckeditor/files/SetcespOnline/Associados/VVLog_-_viavarejo.com.brvvlog.png",
+      address: "Av. Conde Francisco Matarazzo",
+      revenue: 100000
+    }
+
+    await sut.execute(companyData);
+    await expect(sut.execute({
+      name: "vvlog",
+      cnpj: "04221023000500",
+      description: "A transport company in Via's group",
+      logo: "http://www.setcesp.org.br/upload/ckeditor/files/SetcespOnline/Associados/VVLog_-_viavarejo.com.brvvlog.png",
+      address: "Av. Conde Francisco Matarazzo",
+      revenue: 100000
+    })).rejects.toThrowError(Error("User Already Exists"));
+
+  });
+
+})  
