af7d94ac7ea82e5a537e078c154047c98ad7ea4d
josersn <nettorammos@hotmail.com>
2022-03-17 14:33:14 -0300

    test: should be able to edit a company account

---
+++ src/application/modules/account/useCases/editAccount/EditAccountService.spec.ts
@@ -0,0 +1,47 @@
+import { AccountRepository } from "../../repositories/AccountRepository";
+import { CreateAccountService } from "../createAccount/CreateAccountService";
+import { GetAccountService } from "../getAccount/GetAccountService";
+import { EditAccountService } from "./EditAccountService";
+
+let sut: EditAccountService;
+let rut: AccountRepository;
+let accountService: CreateAccountService;
+let getAccountService: GetAccountService;
+
+beforeAll(() => {
+    rut = new AccountRepository();
+    sut = new EditAccountService(rut);
+    accountService = new CreateAccountService(rut);
+    getAccountService = new GetAccountService(rut);
+})
+
+describe('Edit Account company service ', () => {
+    it('Should be to edit a company account', async () => {
+
+        const companyData = {
+            name: "airFox",
+            cnpj: "04221023000500",
+            description: "A digital bank in Via's group",
+            logo: "https://mms.businesswire.com/media/20200522005034/en/793058/23/Airfox_-_Light_Blue_Logo_%282%29.jpg",
+            address: "Av. Conde Francisco Matarazzo",
+            revenue: 100000
+        }
+
+        const newCompanyData = {
+            name: "BanQi",
+            cnpj: "04221023000500",
+            description: "A digital bank in Via's group",
+            logo: "https://banqi.com.br/assets/img/ui/logo-internas.svg",
+            address: "Av. Conde Francisco Matarazzo",
+            revenue: 100000
+        }
+
+        const account = await accountService.execute(companyData);
+        const accountEdited = await sut.execute(account.id, newCompanyData);
+        const accountFound = await getAccountService.execute(accountEdited?.id);
+
+        expect(accountFound?.name).toBe("BanQi");
+        expect(accountFound).toEqual(newCompanyData);
+
+    })
+})
